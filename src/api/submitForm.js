8cb3822c3bf57b0ed6435738f8953cfa1a82becc
Derrick Reimer <derrickreimer@gmail.com>
2019-12-23 11:33:15 -0600

    Refactoring

---
+++ src/api/submitForm.js
@@ -0,0 +1,61 @@
+import Promise from 'promise-polyfill';
+import fetchPonyfill from 'fetch-ponyfill';
+import objectAssign from 'object-assign';
+import { encode, append } from '../util';
+import { version } from '../../package.json';
+
+const serializeBody = data => {
+  if (data instanceof FormData) return data;
+  return JSON.stringify(data);
+};
+
+const submissionUrl = props => {
+  const { id, site, form } = props;
+  const endpoint = props.endpoint || 'https://api.statickit.com';
+
+  if (site && form) {
+    return `${endpoint}/j/sites/${site}/forms/${form}/submissions`;
+  } else {
+    return `${endpoint}/j/forms/${id}/submissions`;
+  }
+};
+
+const clientHeader = ({ clientName }) => {
+  const label = `@statickit/core@${version}`;
+  if (!clientName) return label;
+  return `${clientName} ${label}`;
+};
+
+export default function submitForm(session, props = {}) {
+  if (!props.id && !(props.site && props.form)) {
+    throw new Error('`site` and `form` properties are required');
+  }
+
+  const fetchImpl = props.fetchImpl || fetchPonyfill({ Promise }).fetch;
+  const url = submissionUrl(props);
+  const data = props.data || {};
+  const sessionWithTime = objectAssign({}, session, {
+    submittedAt: 1 * new Date()
+  });
+
+  append(data, '_t', encode(sessionWithTime));
+
+  const request = {
+    method: 'POST',
+    mode: 'cors',
+    body: serializeBody(data),
+    headers: {
+      'StaticKit-Client': clientHeader(props)
+    }
+  };
+
+  if (!(data instanceof FormData)) {
+    request.headers['Content-Type'] = 'application/json';
+  }
+
+  return fetchImpl(url, request).then(response => {
+    return response.json().then(body => {
+      return { body, response };
+    });
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-import { encode, append } from './util';
-
-import Promise from 'promise-polyfill';
-import fetchPonyfill from 'fetch-ponyfill';
-import objectAssign from 'object-assign';
-import { version } from '../package.json';
-
-const serializeBody = data => {
-  if (data instanceof FormData) return data;
-  return JSON.stringify(data);
-};
-
-const submissionUrl = (site, props) => {
-  const { id, form } = props;
-  const endpoint = props.endpoint || 'https://api.statickit.com';
-
-  if (site && form) {
-    return `${endpoint}/j/sites/${site}/forms/${form}/submissions`;
-  } else {
-    return `${endpoint}/j/forms/${id}/submissions`;
-  }
-};
-
-const clientHeader = ({ clientName }) => {
-  const label = `@statickit/core@${version}`;
-  if (!clientName) return label;
-  return `${clientName} ${label}`;
-};
+import submitForm from './api/submitForm';
 
 /**
  * The client constructor.
@@ -59,7 +32,7 @@ function StaticKit(props = {}) {
 /**
  * Tears down the client instance.
  */
-StaticKit.prototype.teardown = function teardown() {
+StaticKit.prototype.teardown = function() {
   window.removeEventListener('mousemove', this._onMouseMove);
   window.removeEventListener('keydown', this._onKeyDown);
 };
@@ -72,40 +45,9 @@ StaticKit.prototype.teardown = function teardown() {
  * @param {object} props
  * @returns {Promise}
  */
-StaticKit.prototype.submitForm = function submitForm(props = {}) {
-  const site = props.site || this.site;
-
-  if (!props.id && !(site && props.form)) {
-    throw new Error('`site` and `form` properties are required');
-  }
-
-  const fetchImpl = props.fetchImpl || fetchPonyfill({ Promise }).fetch;
-  const url = submissionUrl(site, props);
-  const data = props.data || {};
-  const session = objectAssign({}, this.session, {
-    submittedAt: 1 * new Date()
-  });
-
-  append(data, '_t', encode(session));
-
-  const request = {
-    method: 'POST',
-    mode: 'cors',
-    body: serializeBody(data),
-    headers: {
-      'StaticKit-Client': clientHeader(props)
-    }
-  };
-
-  if (!(data instanceof FormData)) {
-    request.headers['Content-Type'] = 'application/json';
-  }
-
-  return fetchImpl(url, request).then(response => {
-    return response.json().then(body => {
-      return { body, response };
-    });
-  });
+StaticKit.prototype.submitForm = function(props = {}) {
+  props.site || (props.site = this.site);
+  return submitForm(this.session, props);
 };
 
 /**
