fd6c0546f102915722293cc363b2f50849ba57a5
Derrick Reimer <derrickreimer@gmail.com>
2020-02-13 16:58:31 -0600

    Add IIFE bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts'];
@@ -18,18 +19,41 @@ const plugins = [
   json()
 ];
 
-export default {
-  input: './src/index.ts',
-  external: ['fetch-ponyfill', 'promise-polyfill'],
-  plugins,
-  output: [
-    {
-      format: 'cjs',
-      file: pkg.main
-    },
-    {
-      format: 'esm',
-      file: pkg.module
+export default [
+  {
+    input: './src/index.ts',
+    external: ['fetch-ponyfill', 'promise-polyfill'],
+    plugins,
+    output: [
+      {
+        format: 'cjs',
+        file: pkg.main
+      },
+      {
+        format: 'esm',
+        file: pkg.module
+      }
+    ]
+  },
+  {
+    input: './src/index.ts',
+    plugins: [
+      babel({
+        extensions,
+        include: ['src/**/*']
+      }),
+      nodeResolve({
+        extensions,
+        browser: true
+      }),
+      commonjs(),
+      json(),
+      terser()
+    ],
+    output: {
+      format: 'iife',
+      name: 'StaticKit',
+      file: 'dist/statickit.iife.js'
     }
-  ]
-};
+  }
+];
